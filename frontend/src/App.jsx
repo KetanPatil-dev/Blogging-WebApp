f2d20b33b0999c3219f93a379cf3303e302b011e
ketan <mail.ketan027@gmail.com>
2025-03-12 14:23:36 +0530

    Implemented navba using bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,39 @@
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import Post from './pages/Post'
-import Register from "./pages/Register"
-import Login from "./pages/Login"
-import Profile from "./pages/Profile"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import UserLayout from "./Layout/UserLayout";
+import AdminLayout from "./Layout/AdminLayout";
+import Dashboard from "./pages/Admin/Dashboard";
+import AddPost from "./pages/Admin/AddPost";
+import Users from "./pages/Admin/Users";
+import AllPosts from "./pages/Admin/AllPosts";
+import "./index.css"
 
 function App() {
-  
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/post/:id" element={<Post/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="profile/:id" element={<Profile/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="profile/:id" element={<Profile />} />
+          </Route>
+          <Route path="/dashboard" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="addpost" element={<AddPost />} />
+            <Route path="users" element={<Users />} />
+            <Route path="allposts" element={<AllPosts />} />
+          </Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav
+      className="navbar d-flex justify-content-between
+  align-items-center p-3"
+    >
+      <Link>
+        <h1 className="mx-5 text-white fs-2 fw-bold">Ketan's Blog</h1>
+      </Link>
+      <div className="d-flex align-items-center">
+        {/* <Link to="/login">
+          <button className="btn_sign mx-3">Sign In</button>
+        </Link> */}
+      </div>
+      <div className="dropdown">
+        <div
+          className="avatar-container pointer rounded-circle overflow-hidden bg-info"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+          style={{ width: "45px", height: "40px", cursor: "pointer" }}
+        >
+          <img
+            src="https://wallpapersok.com/images/high/oscar-zahn-skeleton-headphones-unique-cool-pfp-rboah21ctf7m37o0.webp"
+            alt="ProfileImage"
+            className="img-fluid h-100 w-100"
+            style={{ objectFit: "cover" }}
+          />
+          </div>
+          <ul className="dropdown-menu dropdown-menu-end dropdown-menu-dark">
+            <li><Link className="dropdown-item" to="/dashboard">DashBoard</Link></li>
+            <li><Link className="dropdown-item " to="/profile/989898">Profile</Link></li>
+            <li><Link className="dropdown-item" style={{cursor:"pointer"}}>Sign Out</Link></li>
+           
+          </ul>
+        </div>
+      
+    </nav>
+  );
+};
+
+export default NavBar;
